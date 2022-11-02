7ea49f05195723c6c84dccdea5b06f2241bda568
JD_Rodrigues <jdevproject01@gmail.com>
2022-11-02 20:39:29 -0300

    Criação das rotas

---
+++ src/pages/about/about.jsx
@@ -0,0 +1,10 @@
+
+
+export const About = () => {
+
+  return(
+    <div>
+      <h1>Sobre</h1>
+    </div>
+  )
+}
---
+++ src/pages/history/history.jsx
@@ -0,0 +1,8 @@
+export const History = () => {
+
+  return(
+    <div>
+      <h1>Histórico de busca</h1>
+    </div>
+  )
+}
