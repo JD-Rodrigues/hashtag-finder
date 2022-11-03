badebe8c3b154b13003fb37e2fefa9b0becaab11
JD_Rodrigues <jdevproject01@gmail.com>
2022-11-02 21:53:17 -0300

    reset css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import css from './main.module.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
