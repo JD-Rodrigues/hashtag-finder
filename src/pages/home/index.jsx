ca212a4f0355d90dd4e8d1bbce8cc6e218e29294
JD_Rodrigues <jdevproject01@gmail.com>
2022-11-04 15:18:46 -0300

    Criação do texto da área de busca

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,26 @@
 import { FirstSection } from "../../components/firstSection"
 import { Footer } from "../../components/footer"
 import styles from "./styles.module.css"
+import searchIcon from "../../../public/images/icons/icon-search.svg"
+
 
 export const Home = () => {
 
   return(
     <div className={styles.main}>
       <FirstSection>
-        <h1>Página inicial</h1>
+        <div className={styles.search__section}>
+          <div className={styles.search__section__text}>
+            <h1 className={styles.title}>Encontre hashtags de maneira fácil.</h1>
+            <p className={styles.description}>Digite o que deseja no campo de buscas e confira os resultados do Twitter abaixo.</p>
+          </div>
+          <div className={styles.search__wrapper}>
+            <img src={searchIcon} alt="" />
+            <form action="">
+              <input type="search" placeholder="Buscar..." />
+            </form>
+          </div>
+        </div>
       </FirstSection>
     </div>
   )
