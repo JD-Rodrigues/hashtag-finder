07cb0dee041b471d15d6d701458a8009f0d3dc12
JD_Rodrigues <jdevproject01@gmail.com>
2022-11-07 16:04:33 -0300

    Implementation - Componente TweetCard e área de resultados de busca que renderizará os tweets

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,10 +2,12 @@ import { FirstSection } from "../../components/firstSection"
 import { Footer } from "../../components/footer"
 import styles from "./styles.module.css"
 import searchIcon from "../../../public/images/icons/icon-search.svg"
+import { useState } from "react"
+import { TweetCard } from "../../components/tweetCard"
 
 
 export const Home = () => {
-
+  const [resultTab, setResultTab] = useState('Tweets')
   return(
     <div className={styles.main}>
       <FirstSection>
@@ -20,14 +22,33 @@ export const Home = () => {
               <input className={styles.search} type="search" placeholder="Buscar..." />
             </form>
           </div>
-          {/* <p className={styles.results__subtitle}>
-              Exibindo os 10 resultados mais recentes para 
-              <span> 
-                {` #natureza`}
-              </span>
-          </p> */}
         </div>
       </FirstSection>
+      <section className={styles.search__result}>
+        <p className={styles.result__subtitle}>
+          Exibindo os 10 resultados mais recentes para 
+          <span> 
+            {` #natureza`}
+          </span>
+        </p>  
+        <div className={styles.result__tabs}>
+          <div className={resultTab === 'Tweets' ? styles.active__tab : styles.tab__tweets}>Tweets</div>
+          <div className={resultTab === 'Images' ? styles.active__tab : styles.tab__images}>Imagens</div>
+        </div>
+        <div className={styles.result__tweets}>
+          <TweetCard />
+          <TweetCard />
+          <TweetCard />
+          <TweetCard />
+          <TweetCard />
+          <TweetCard />
+          <TweetCard />
+          <TweetCard />
+          <TweetCard />
+          <TweetCard />
+        </div>
+        
+      </section>
     </div>
   )
 }
