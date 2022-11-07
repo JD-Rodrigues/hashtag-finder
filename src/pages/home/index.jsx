8e5cb71c97e6a98d463dee4e39c4f5409e5c73e2
JD_Rodrigues <jdevproject01@gmail.com>
2022-11-07 12:23:21 -0300

    Responsividade para telas intermediárias (768, 1112, 1500)

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -16,7 +16,7 @@ export const Home = () => {
           </div>
           <div className={styles.search__wrapper}>
             <img src={searchIcon} className={styles.search__icon} alt="" />
-            <form action="">
+            <form className={styles.form__search} action="">
               <input className={styles.search} type="search" placeholder="Buscar..." />
             </form>
           </div>
