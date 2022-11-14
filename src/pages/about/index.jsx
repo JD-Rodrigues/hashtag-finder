207d4d29ac0370f9fea9220be488845a1a42e52e
Brakinveste <brakinveste@gmail.com>
2022-11-14 13:21:14 -0300

    revisão 8  final-1.0

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from "./about.module.css"
 import ilustration from '/public/images/icons/about-ilustration.svg'
-import logo from '/images/icons/logo.svg'
-import userIcon from '/images/icons/icon-user-alt.svg'
-import infoIcon from '/images/icons/icon-info-circle.svg'
+//import logo from '/images/icons/logo.svg'
+// import userIcon from '/images/icons/icon-user-alt.svg'
+//import infoIcon from '/images/icons/icon-info-circle.svg'
 import git from '/public/images/icons/icon-github.svg'
 import email from '/public/images/icons/icon-envelope.svg'
 import linkedin from '/public/images/icons/icon-awesome-linkedin.svg'
 import { time } from '../../services'
+ 
 
 
 
@@ -20,16 +21,10 @@ export const About = () => {
           <div className={styles.text__box} >
             <h1 className={styles.text__title} >Sobre o projeto</h1>
             <p className={styles.text__about} >
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
-            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam 
-            voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit 
-            amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed 
-            diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-              clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
-              eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam</p>
+            
+              
+
+            </p>
           </div>
           
           <div className={styles.img__decor} > 
