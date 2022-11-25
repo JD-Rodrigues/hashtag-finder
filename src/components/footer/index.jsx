57bca6dc02f8337224279b89c5143647affa7baa
Brakinveste <brakinveste@gmail.com>
2022-11-25 15:20:58 -0300

    ok

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css"
 export const Footer = () => {
   return(
     <footer id="end__list__tweets" className={styles.footer}>
-      <p>@Newtab Academy 2022. Todos os direitos reservados</p>
+      <p className={styles.footer__text} >@Newtab Academy 2022. Todos os direitos reservados</p>
     </footer>
   )
 }
