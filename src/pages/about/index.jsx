2592a54aff8dda06506f79ea8a385a6a5c363291
Brakinveste <brakinveste@gmail.com>
2022-11-25 15:21:59 -0300

    acertando o css

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,19 +1,8 @@
 import React, { useState } from 'react'
 import { Helmet } from 'react-helmet'
 import styles from './about.module.css'
-//import useDocumentTitle from "@tanem/use-document-title";
-
-
-//import aboutTextBlock from '../../services'
-
-
-
 
 import ilustration from '/public/images/icons/about-ilustration.svg'
-//import logo from '/images/icons/logo.svg'
-// import userIcon from '/images/icons/icon-user-alt.svg'
-//import infoIcon from '/images/icons/icon-info-circle.svg'
-// import git from '/public/images/icons/icon-github.svg'
 import email from '/public/images/icons/icon-envelope.svg'
 import linkedin from '/public/images/icons/icon-awesome-linkedin.svg'
 import { initialStateMembers, listMembers} from '../../services'
@@ -94,79 +83,7 @@ export const About =  () => {
       return <li key={index}><CardMember member={member} /></li>
     })}
               
-              {/* <div className={styles.cards} >
-                <div id='card__pic' className={styles.card__pic01} ></div>
-                <h2 className={styles.card__name} >{ time[0].Nome}</h2>
-                <p className={styles.card__text} >
-                  {time[0].Descrição}
-                </p>
-                <div className={styles.icons__list}>
-                  <a href={time[0].Github} target='_blank' >
-                  <img src={git} className={styles.icons__git} ></img>
-                  </a>
-                  <a href={`mailto:${time[0].Email}`} target='_blank' >
-                  <img src={email} className={styles.icons__mail} ></img>
-                  
-                  </a>
-                  <a href={time[0].LinkedIn} target='_blank' >
-                  <img src={linkedin} className={styles.icons__linkedin} ></img>
-                  </a>
-                </div>
-              </div>             
-              <div className={styles.cards} >
-                <div className={styles.card__pic02} ></div>
-                <h2 className={styles.card__name} > {time[1].Nome} </h2>
-                <p className={styles.card__text} >
-                  {time[1].Descrição}
-                </p>
-                <div className={styles.icons__list}>
-                <a href={time[1].Github} target='_blank' >
-                  <img src={git} className={styles.icons__git} ></img>
-                  </a>
-                  <a href={`mailto:${time[1].Email}`}  target='_blank' >
-                  <img src={email} className={styles.icons__mail} ></img>
-                  </a>
-                  <a href={time[1].LinkedIn} target='_blank' >
-                  <img src={linkedin} className={styles.icons__linkedin} ></img>
-                  </a>
-                </div>
-              </div>  
-              <div className={styles.cards} >
-                <div className={styles.card__pic03} ></div>
-                <h2 className={styles.card__name} >{time[2].Nome}</h2>
-                <p className={styles.card__text} >
-                  {time[2].Descrição}
-                </p>
-                <div className={styles.icons__list}>
-                <a href={time[2].Github} target='_blank' >
-                  <img src={git} className={styles.icons__git} ></img>
-                  </a>
-                  <a href={`mailto:${time[2].Email}`}  target='_blank' >
-                  <img src={email} className={styles.icons__mail} ></img>
-                  </a>
-                  <a href={time[2].LinkedIn} target='_blank' >
-                  <img src={linkedin} className={styles.icons__linkedin} ></img>
-                  </a>
-                </div>
-              </div>  
-              <div className={styles.cards} >
-                <div className={styles.card__pic04} ></div>
-                <h2 className={styles.card__name} >{time[3].Nome}</h2>
-                <p className={styles.card__text} >
-                  {time[3].Descrição}
-                </p>
-                <div className={styles.icons__list}>
-                <a href={time[3].Github} target='_blank' >
-                  <img src={git} className={styles.icons__git} ></img>
-                  </a>
-                  <a href={`mailto:${time[3].Email}`}  target='_blank' >
-                  <img src={email} className={styles.icons__mail} ></img>
-                  </a>
-                  <a href={time[3].LinkedIn} target='_blank' >
-                  <img src={linkedin} className={styles.icons__linkedin} ></img>
-                  </a>
-                </div>
-            </div>  */}
+              
             </ul> 
           
 
